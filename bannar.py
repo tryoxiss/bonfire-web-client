e9f625fbe7811623f158f32a5c84e568da61ac06
tryoxiss <tryoxiss@gmail.com>
2023-02-09 18:25:39 -0800

    Badges

---
+++ bannar.py
@@ -0,0 +1,22 @@
+
+
+def is_true(variable: bool): 
+    if variable == True: 
+        print("True")
+    elif variable == False: 
+        print("False")
+    else:
+        print("This code is useless")
+
+
+
+
+
+
+
+
+
+
+
+
+        
